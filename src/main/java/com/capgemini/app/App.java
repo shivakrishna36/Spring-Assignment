bae08b1cf77550213d58637ba57c75a0120a832e
GUNJI <shivakrishna.gunji@capgemini.com>
2019-01-04 09:54:43 +0530

    Adds spring assignment

--- src/main/java/com/capgemini/app/App.java
+++ src/main/java/com/capgemini/app/App.java
@@ -1,18 +1,15 @@
 package com.capgemini.app;
 
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.capgemini.app.bean.Organization;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-     
-      ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-      Organization organization = (Organization) context.getBean("organization");
-      System.out.println(organization.toString());		
-    }
+public class App {
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+		Organization organization = (Organization) context.getBean("organization");
+		System.out.println(organization.toString());
+	}
 }
